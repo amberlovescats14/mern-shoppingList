187e3dbc779da91455ea8970e683f45cf7c5de80
Amber Jones <amberlovescats14@gmail.com>
2019-06-12 09:14:45 -0500

    finished

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://brad:brad123@mernshopping-e3knb.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+
+const app = express();
+
+const items = require('./routes/api/items')
+
+
+//bodyparser middleware
+app.use(bodyParser.json());
+
+//DB Config
+const db = require('./config/keys').mongoURI
+
+//connect to Mongo
+mongoose.connect(db)
+.then(()=> console.log('MONGO DB Connected'))
+.catch(err => console.log(err))
+
+// USER ROUTES
+app.use('/api/items', items)
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=> console.log(`Server started on port: ${port}`))
